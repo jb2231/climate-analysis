8006d7f0ac13d6992d28f3a82ad15cdf83d6cf2f
Jack Bragg <jb2231@kent.ac.uk>
2024-11-20 11:12:56 +0000

    Add rainfall processign placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -26,3 +26,4 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+# TODO (jb2231) : Add call to process rainfall
